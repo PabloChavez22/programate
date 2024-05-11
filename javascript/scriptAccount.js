8eb73b03bd8ca9121883d91b62e8008b6cd8b68d
PabloChavez22 <119630058+PabloChavez22@users.noreply.github.com>
2024-05-11 19:26:20 -0300

    Add function in scriptAccount.js
    agregando funcion de paises y confirmacion de contraseña

--- javascript/scriptAccount.js
+++ javascript/scriptAccount.js
@@ -22,3 +22,42 @@ btn2.addEventListener("click", function() {
 
     titulo.innerHTML = 'Iniciar sesion';
 });
+
+
+/************************************************************
+* formulario                                                *  
+************************************************************/
+
+const opcionesPais = document.getElementById("pais");
+const campoTexto = document.getElementById("otro-pais");
+const texto = document.getElementById("nombrePais");
+campoTexto.style.display = 'none';
+opcionesPais.addEventListener("change",function(){
+    if(opcionesPais.value === "otro"){
+        campoTexto.style.display = "block";
+    }else{
+        campoTexto.style.display = "none";
+        texto.value = "";
+    }
+});
+
+/******validacion contra **********/
+
+const passInput = document.getElementById("passw");
+const confirm = document.getElementById("passw-two");
+
+passInput.addEventListener("input",()=>{  
+  passInput.value = passInput.value.trim();//elimina espacios en blanco al escribir.
+});
+
+confirm.addEventListener("input",()=>{ 
+
+  confirm.value = confirm.value.trim();
+
+  if(passInput.value === confirm.value){
+    confirm.setCustomValidity("");//coinciden
+  }else{
+    confirm.setCustomValidity("Las contraseñas no coinciden");
+    
+  }
+});
