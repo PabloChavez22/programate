d7219d57b5ce5a3189c20d615f72fcd4ccefa6b3
PabloChavez22 <119630058+PabloChavez22@users.noreply.github.com>
2024-05-22 21:20:58 -0300

    Update scriptAccount.js
    agregando funciones para etiquetas a

--- javascript/scriptAccount.js
+++ javascript/scriptAccount.js
@@ -61,3 +61,51 @@ confirm.addEventListener("input",()=>{
     
   }
 });
+
+/*****************modal***************/
+// Obtener los elementos del DOM
+const modal1 = document.getElementById('ayuda1');
+const modal2 = document.getElementById('privacidad1');
+const modal3 = document.getElementById('condiciones1');
+
+const openModal1 = document.getElementById('ayuda2');
+const openModal2 = document.getElementById('privacidad2');
+const openModal3 = document.getElementById('condiciones2');
+
+const closeModal1 = document.getElementById('ayuda3');
+const closeModal2 = document.getElementById('privacidad3');
+const closeModal3 = document.getElementById('condiciones3');
+
+
+// Function para abrir modal.
+function openModal(modal) {
+  modal.style.display = 'block';
+}
+
+// Function para cerrar modal.
+function closeModal(modal) {
+  modal.style.display = 'none';
+}
+
+// Abrir los modals.
+openModal1.onclick = () => openModal(modal1);
+openModal2.onclick = () => openModal(modal2);
+openModal3.onclick = () => openModal(modal3);
+
+
+// Cerrar los modals al clic en cerrar.
+closeModal1.onclick = () => closeModal(modal1);
+closeModal2.onclick = () => closeModal(modal2);
+closeModal3.onclick = () => closeModal(modal3);
+
+
+// Cerrar los modals al clic fuera.
+window.onclick = (event) => {
+  if (event.target === modal1) {
+    closeModal(modal1);
+  } else if (event.target === modal2) {
+    closeModal(modal2);
+  }else if (event.target === modal3) {
+    closeModal(modal3);
+  }
+};
